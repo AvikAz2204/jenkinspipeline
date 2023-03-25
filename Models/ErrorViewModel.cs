f8a87c5941d3677a36371ae34c4fbc287667e430
unknown <avik.ghosh22@gmail.com>
2023-03-25 15:29:39 +0530

    solution added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JenkinsBuildPipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
