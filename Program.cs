9066519ab28d4ebb8ef1c4c74ee33253fa0cad14
avikaz2204 <avikaz2204ghosh@outlook.com>
2023-03-31 11:47:15 +0530

    simple commit docker with prometheus

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Prometheus;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,7 +17,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
+app.UseHttpMetrics();
+app.UseMetricServer();
 app.UseRouting();
 
 app.UseAuthorization();
