98d93121304ec939fb82261906ea3c77158b2cdf
avikaz2204 <avikaz2204ghosh@outlook.com>
2023-04-04 15:29:34 +0530

    simple commit docker

--- Program.cs
+++ Program.cs
@@ -1,13 +1,39 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+
 using Prometheus;
+using App.Metrics;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddMetrics();
+
+var metrics = AppMetrics.CreateDefaultBuilder().Build();
 
 
+
+builder.Services.AddMetrics(metrics);
+builder.Services.AddMetricsTrackingMiddleware();
+
+var snapshot = metrics.Snapshot.Get();
+
+foreach (var formatter in metrics.OutputMetricsFormatters)
+{
+    using (var stream = new MemoryStream())
+    {
+        await formatter.WriteAsync(stream, snapshot);
+
+        var result = Encoding.UTF8.GetString(stream.ToArray());
+
+        System.Console.WriteLine(result);
+    }
+}
 
 var app = builder.Build();
 
@@ -21,8 +47,19 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+//app.UseMetricsApdexTrackingMiddleware();
+//app.UseMetricsRequestTrackingMiddleware();
+//app.UseMetricsErrorTrackingMiddleware();
+//app.UseMetricsActiveRequestMiddleware();
+//app.UseMetricsPostAndPutSizeTrackingMiddleware();
+//app.UseMetricsOAuth2TrackingMiddleware();
+
 app.UseHttpMetrics();
+app.UseMetricsTextEndpoint();
+app.UseMetricsAllMiddleware();
 app.UseMetricServer();
+
 
 app.UseRouting();
 
@@ -31,5 +68,8 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
 
 app.Run();
